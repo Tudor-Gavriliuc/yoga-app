3a89f588eaa7e8419d695fd40806804a2f1157a1
Tudor_889 <sigmoidtudor@gmail.com>
2026-06-20 13:01:54 +0300

    Add automated migrations to Dockerfile

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\SetLocale;
+use App\Http\Middleware\TrustProxies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
             SetLocale::class,
+        ]);
+
+        $middleware->trustProxies(append: [
+            TrustProxies::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
